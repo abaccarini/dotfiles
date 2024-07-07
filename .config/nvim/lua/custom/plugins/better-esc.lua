ea278d887c678424e51757cba76aad8f49b078b9
Alessandro Baccarini <anbaccar@buffalo.edu>
2024-07-07 15:37:24 -0400

    updated better esc

--- .config/nvim/lua/custom/plugins/better-esc.lua
+++ .config/nvim/lua/custom/plugins/better-esc.lua
@@ -3,7 +3,17 @@ return {
   'max397574/better-escape.nvim',
   config = function()
     require('better_escape').setup {
-      mapping = { 'jj' }, -- a table with mappings to use
+      -- mapping = { 'jj' }, -- a table with mappings to use
+      -- mappings = {
+      --   i = {
+      --     j = {
+      --       -- These can all also be functions
+      --       k = '<Esc>',
+      --       j = '<Esc>',
+      --     },
+      --   },
+
+      -- },
     }
   end,
 }
