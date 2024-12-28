144ce8ae201150dd64a0f378b2bd81045e39a283
Alessandro Baccarini <baccaan@gmail.com>
2024-12-27 21:23:02 -0500

    adding new plugin

---
+++ .config/nvim/lua/custom/plugins/bullets.lua
@@ -0,0 +1,12 @@
+return {
+  'bullets-vim/bullets.vim',
+  -- name = 'barbecue',
+  -- version = '*',
+  -- dependencies = {
+  --   'SmiteshP/nvim-navic',
+  --   'nvim-tree/nvim-web-devicons', -- optional dependency
+  -- },
+  -- opts = {
+    -- configurations go here
+  -- },
+}
