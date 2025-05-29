de28a3d30b0a3f70188b4ef545c13150b4c1f1ad
Alessandro Baccarini <baccaan@gmail.com>
2025-05-28 22:24:14 -0400

    whichkey update

--- .config/nvim/lua/plugins/whichkey.lua
+++ .config/nvim/lua/plugins/whichkey.lua
@@ -1,12 +1,31 @@
 return { -- Useful plugin to show you pending keybinds.
   -- enabled = false,
   'folke/which-key.nvim',
+  -- dependencies = { {'echasnovski/mini.icons', version = false} },
   event = 'VimEnter', -- Sets the loading event to 'VimEnter'
+  keys = {
+    {
+      '<leader>?',
+      function()
+        require('which-key').show { global = false }
+      end,
+      desc = 'Buffer Local Keymaps (which-key)',
+    },
+  },
   config = function() -- This is the function that runs, AFTER loading
     -- vim.keymap.set('n', '<leader>S', 'z=', {remap = true})
     -- vim.keymap.set('n', '<leader>Sa', 'zg', {remap = true})
     -- vim.keymap.set('n', '<leader>su', 'zug', {remap = true})
-    require('which-key').setup()
+    require('which-key').setup {
+      preset = 'helix',
+      icons = {
+        colors = true,
+      },
+      -- keys = {
+      --   scroll_up = '<c-k>', -- binding to scroll up inside the popup
+      --   scroll_down = '<c-j>', -- binding to scroll down inside the popup
+      -- },
+    }
 
     -- Document existing key chains
     -- require('which-key').register {
